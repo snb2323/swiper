5dbc8659a7339c6a19ef7c8f5ce5de8154dd69cc
User -2023WJDST\Administrator <dheltus1r@naver.com>
2023-11-06 11:03:17 +0900

    상하슬라이드

---
+++ js/wed.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    //화면이 준비되면 실행해라.
+    // setInterval , animate , css , appendTo
+    // setInterval(function () { }, 3000)
+    //$(태그).animate({},speed , function(){})
+    //$(태그).css(스타일,값)
+    //$(옮길태그).appendTo(부모가될태그)
+
+
+    const aniw = $(".swiper-slide").height();
+
+    setInterval(function () {
+        $(".swiper-wrapper").animate({ "margintop": -aniw }, 400, function () {
+            $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
+            $(".swiper-wrapper").css("marginLeft", 0)
+        })
+    }, 3000)
+
+})
